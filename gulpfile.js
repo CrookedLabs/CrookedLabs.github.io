4557b84245e1194ed33466e107a31855f5aa5985
Chris Sciolla <csciolla1@gmail.com>
2016-01-06 16:39:36 -0500

    add assets task to browsersync task

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
 * Wait for jekyll-build, then launch the Server
 */
-gulp.task('browser-sync', ['sass', 'js', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['sass', 'js', 'assets', 'jekyll-build'], function() {
   browserSync({
     server: {
       baseDir: '_site'
